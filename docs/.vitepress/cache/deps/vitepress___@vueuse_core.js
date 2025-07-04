4f56b5029b2b7d8d194b5888f719b0d4a41837c6
WangYuxiang <3062871098@qq.com>
2025-07-04 14:44:00 +0800

    wyx

--- docs/.vitepress/cache/deps/vitepress___@vueuse_core.js
+++ docs/.vitepress/cache/deps/vitepress___@vueuse_core.js
@@ -274,8 +274,8 @@ import {
   watchTriggerable,
   watchWithFilter,
   whenever
-} from "./chunk-YJ6QP2VR.js";
-import "./chunk-LW4I4DCF.js";
+} from "./chunk-4XTXHTKI.js";
+import "./chunk-X54IR6VG.js";
 export {
   DefaultMagicKeysAliasMap,
   StorageSerializers,
--- docs/.vitepress/cache/deps/vitepress___@vueuse_integrations_useFocusTrap.js
+++ docs/.vitepress/cache/deps/vitepress___@vueuse_integrations_useFocusTrap.js
@@ -3,14 +3,14 @@ import {
   toValue,
   tryOnScopeDispose,
   unrefElement
-} from "./chunk-YJ6QP2VR.js";
+} from "./chunk-4XTXHTKI.js";
 import {
   computed,
   ref,
   watch
-} from "./chunk-LW4I4DCF.js";
+} from "./chunk-X54IR6VG.js";
 
-// node_modules/tabbable/dist/index.esm.js
+// node_modules/.pnpm/tabbable@6.2.0/node_modules/tabbable/dist/index.esm.js
 var candidateSelectors = ["input:not([inert])", "select:not([inert])", "textarea:not([inert])", "a[href]:not([inert])", "button:not([inert])", "[tabindex]:not(slot):not([inert])", "audio[controls]:not([inert])", "video[controls]:not([inert])", '[contenteditable]:not([contenteditable="false"]):not([inert])', "details>summary:first-of-type:not([inert])", "details:not([inert])"];
 var candidateSelector = candidateSelectors.join(",");
 var NoElement = typeof Element === "undefined";
@@ -341,7 +341,7 @@ var isFocusable = function isFocusable2(node, options) {
   return isNodeMatchingSelectorFocusable(options, node);
 };
 
-// node_modules/focus-trap/dist/focus-trap.esm.js
+// node_modules/.pnpm/focus-trap@7.6.2/node_modules/focus-trap/dist/focus-trap.esm.js
 function _arrayLikeToArray(r, a) {
   (null == a || a > r.length) && (a = r.length);
   for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e];
@@ -1041,7 +1041,7 @@ var createFocusTrap = function createFocusTrap2(elements, userOptions) {
   return trap;
 };
 
-// node_modules/@vueuse/integrations/useFocusTrap.mjs
+// node_modules/.pnpm/@vueuse+integrations@11.3.0_d8c353b90272bb2254cb1fb907ecd39a/node_modules/@vueuse/integrations/useFocusTrap.mjs
 function useFocusTrap(target, options = {}) {
   let trap;
   const { immediate, ...focusTrapOptions } = options;
